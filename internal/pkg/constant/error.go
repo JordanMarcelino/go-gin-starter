f72133d66c0f4e9d3bcec9a112d1473e5c33801e
JordanMarcelino <jhordan.marcelino@gmail.com>
2024-11-26 23:16:05 +0700

    feat: add custom errors

---
+++ internal/pkg/constant/error.go
@@ -0,0 +1,10 @@
+package constant
+
+const (
+	InternalServerErrorMessage = "currently our server is facing unexpected error, please try again later"
+	EOFErrorMessage            = "missing body request"
+	JsonSyntaxErrorMessage     = "invalid JSON syntax"
+	UnauthorizedErrorMessage   = "unauthorized"
+	RequestTimeoutErrorMessage = "failed to process request in time, please try again"
+	ValidationErrorMessage     = "input validation error"
+)
